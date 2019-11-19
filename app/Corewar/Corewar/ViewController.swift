8c34a24ef585eb5ab6d6cc18d7afd48561af38dd
horiz0n-zero <antoinefeufeu>
2019-11-19 07:32:18 +0100

    app

--- app/Corewar/Corewar/ViewController.swift
+++ app/Corewar/Corewar/ViewController.swift
@@ -8,6 +8,31 @@
 
 import Cocoa
 
+class Libcorewar: NSObject {
+    
+    enum LibcorewarError: Error {
+        case content(String)
+        var localizedDescription: String {
+            switch self {
+            case .content(let txt):
+                return txt
+            }
+        }
+    }
+    
+    func getSrc(named name: String) throws -> s_libcorewar_src_file {
+        var error: UnsafeMutablePointer<Int8>? = nil
+        let named = name.cString(using: .utf8)!
+        let srcFile: UnsafeMutablePointer<s_libcorewar_src_file>? = libcorewar_get_src_file(named, &error)
+        
+        if error != nil {
+            throw LibcorewarError.content(String(cString: error!))
+        }
+        return srcFile!.pointee
+    }
+    
+}
+
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
